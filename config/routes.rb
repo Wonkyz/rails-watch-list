5b70820a247b60b88f9e44b47dd62ceace93f375
Bruno Lopes lopesbruno90@gmail.com <lopesbruno90@gmail.com>
2025-05-30 15:04:57 +0200

    cinema

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,13 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :lists, only: [:index, :show, :new, :create] do
+  resources :bookmarks, only: [:new, :create]
+  resources :reviews, only: [:create]
+  end
+
+  resources :bookmarks, only: [:destroy]
+  resources :movies, only: [:index]
+
+  root to: "lists#index"
 end
